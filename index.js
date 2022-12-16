59459ef82531ac2ea8d788018abff48a8ba4ce92
Colton Brenneman <colton.brenneman@gmail.com>
2022-12-15 22:53:33 -0700

    commit tree 75aa954b8ae8b87d16fe47a17c62d09ed7b1ad52

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(name="roller-skate")
+{
+    return `This Saturday, I want to ${name}!`;
+}
+
+function mondayWork(name="go to the office")
+{
+    return `This Monday, I will ${name}.`;
+}
+
+function wrapAdjective(operator="*"){
+    return function (value ="special"){
+        return `You are ${operator}${value}${operator}!`;
+    }
+}
